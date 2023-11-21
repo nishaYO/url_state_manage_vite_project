bdf62989173779c5c61e8a0adb9ff1d0d4ad9c68
Nisha Singhal <nishasinghal979@gmail.com>
2023-11-21 14:40:44 +0530

    restructure code

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,32 +2,29 @@ import { navigate } from "wouter/use-location";
 import { useState } from "react";
 
 function Home() {
-    const handleButtonClick = (id) => {
-        const button = document.getElementById(id);
-        const text = button.innerText;
-        button.innerText = text === "Select Me" ? "Selected" : "Select Me";
-      };
+  const initialButtonIds = [100, 200, 300, 400, 500];
+  const [selectedButtons, setSelectedButtons] = useState([]); // array to contain all selected buttons
+
+  const handleButtonClick = (id) => {
+    setSelectedButtons((prevSelectedButtons) => {
+      if (prevSelectedButtons.includes(id)) {
+        return prevSelectedButtons.filter((buttonId) => buttonId !== id);
+      } else {
+        return [...prevSelectedButtons, id];
+      }
+    });
+  };
 
   return (
     <div>
       <h1>Home Page</h1>
-      <button  onClick={() => navigate("/")}>Go Back</button>
-      <div style={{ padding: "20px", margin: "20px",  display: "flex", gap: "20px", flexWrap: "wrap" }}>
-        <button id="100" onClick={() => handleButtonClick("100")}>
-          Select Me
-        </button>
-        <button id="200" onClick={() => handleButtonClick("200")}>
-          Select Me
-        </button>
-        <button id="300" onClick={() => handleButtonClick("300")}>
-          Select Me
-        </button>
-        <button id="400" onClick={() => handleButtonClick("400")}>
-          Select Me
-        </button>
-        <button id="500" onClick={() => handleButtonClick("500")}>
-          Select Me
-        </button>
+      <button onClick={() => navigate("/")}>Go Back</button>
+      <div style={{ padding: "20px", margin: "20px", display: "flex", gap: "20px", flexWrap: "wrap" }}>
+        {initialButtonIds.map((id) => (
+          <button key={id} id={id} onClick={() => handleButtonClick(id)} className={selectedButtons.includes(id) ? "selected" : ""}>
+            {selectedButtons.includes(id) ? "Selected" : "Select Me"}
+          </button>
+        ))}
       </div>
     </div>
   );
