5e1cd12c58fb72b2ce962337972515fb82f22568
Nisha Singhal <nishasinghal979@gmail.com>
2023-11-21 12:39:29 +0530

    Add buttons to home page and style it

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,35 @@
 import { navigate } from "wouter/use-location";
+import { useState } from "react";
 
 function Home() {
+    const handleButtonClick = (id) => {
+        const button = document.getElementById(id);
+        const text = button.innerText;
+        button.innerText = text === "Select Me" ? "Selected" : "Select Me";
+      };
+
   return (
-    <>
+    <div>
       <h1>Home Page</h1>
       <button  onClick={() => navigate("/")}>Go Back</button>
-    </>
+      <div style={{ padding: "20px", margin: "20px",  display: "flex", gap: "20px", flexWrap: "wrap" }}>
+        <button id="100" onClick={() => handleButtonClick("100")}>
+          Select Me
+        </button>
+        <button id="200" onClick={() => handleButtonClick("200")}>
+          Select Me
+        </button>
+        <button id="300" onClick={() => handleButtonClick("300")}>
+          Select Me
+        </button>
+        <button id="400" onClick={() => handleButtonClick("400")}>
+          Select Me
+        </button>
+        <button id="500" onClick={() => handleButtonClick("500")}>
+          Select Me
+        </button>
+      </div>
+    </div>
   );
 }
 
